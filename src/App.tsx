2952199c51bad9b4001642b03d4d7e88347eab4f
duclhph20070 <duclhph20070@fpt.edu.vn>
2023-07-24 20:26:59 +0700

    push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import ProductList from "./components/ProductList";
+import "react-loading-skeleton/dist/skeleton.css";
+
+function App() {
+    return (
+        <div>
+            <ProductList />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/ProductList/index.tsx
@@ -0,0 +1,45 @@
+import { instance } from "@/axios/config";
+import { useEffect } from "react";
+import Skeleton from "react-loading-skeleton";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "..";
+import { addProduct, fetchProducts, removeProduct, updateProduct } from "@/actions/product";
+import { Dispatch } from "redux";
+
+const ProductList = () => {
+    const dispatch: Dispatch<any> = useDispatch();
+    const { products, isLoading, error } = useSelector((state: any) => state.products);
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
+
+    if (isLoading) return <Skeleton count={3} height={35} />;
+    if (error) return <div>{error}</div>;
+    return (
+        <div>
+            {products?.map((item: any) => {
+                return <div key={item.id}>{item.name}
+                    {}
+                </div>;
+            })}
+
+            <Button type="primary" onClick={() => addProduct({ name: "Product C" })}>
+                Add Product
+            </Button>
+
+            <Button
+                type="primary"
+                onClick={() => updateProduct({ name: "Product C updated ", id: 3 })}
+            >
+                Update Product
+            </Button>
+            <Button type="primary" onClick={() => removeProduct({ id: 3 })}>
+                Delete Product
+            </Button>
+
+        </div>
+    );
+};
+
+export default ProductList;
